a05d16d57eb9819cad34d4a6a596af2b4070db84
KekeliVa <kekeli.afantchao@yahoo.com>
2020-09-13 13:41:11 -0400

    attempting to fix html_routes.js w/ BCS review

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const notes = require("../db/store");
+const path = require("path");
 
 // GET request for /notes, returning notes.html file
 router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 // Dependencies
 const express = require("express");
-const app = express;
+const app = express();
 const PORT = process.env.PORT || 3000;
-const htmlRoutes = require("./routes/html_routes");
+const htmlRoutes = require("./routes/html_routes");  
 const apiRoutes = require("./routes/api_routes");
+
 
 // GET /notes, return notes.html file
 app.use(express.json());
@@ -15,3 +16,7 @@ app.use("/", htmlRoutes);
 // update hot restaurant and refer to it
 // fill out rest of api routes
 // fill out html routes to actually see the renders
+
+app.listen(PORT, () => {
+  console.log(`App is currently listening at http://localhost${PORT}`)
+})
